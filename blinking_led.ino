4c73d46fa050b86eda53b3af30958b371a6e2365
Tarekmohamed97 <36452955+Tarekmohamed97@users.noreply.github.com>
2018-02-26 22:38:32 +0200

    Add files via upload

---
+++ blinking_led.ino
@@ -0,0 +1,16 @@
+const int LED = 13;
+void setup() 
+{
+pinMode(LED,OUTPUT);  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+ digitalWrite(LED,HIGH);
+ delay(1000);
+ 
+ digitalWrite(LED,LOW);
+ delay(1000);// put your main code here, to run repeatedly:
+
+}
