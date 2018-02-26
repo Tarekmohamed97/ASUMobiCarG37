5e111503b6117a29c0a08f7a7685201166e88cd3
Tarekmohamed97 <36452955+Tarekmohamed97@users.noreply.github.com>
2018-02-26 22:38:09 +0200

    Add files via upload

---
+++ blinking_led_with_push_button.ino
@@ -0,0 +1,24 @@
+const int LEDPin = 13;
+const int buttonpin = 2;
+int val;
+void setup() 
+{
+ pinMode(LEDPin,OUTPUT);
+ pinMode(buttonpin,INPUT);// put your setup code here, to run once:
+
+}
+
+void loop() 
+{
+ val = digitalRead(buttonpin);
+ if (val == HIGH )
+ {
+  digitalWrite(LEDPin,HIGH);
+  delay(1000);
+  digitalWrite(LEDPin,LOW);
+  delay(1000);
+  }// put your main code here, to run repeatedly:
+else {
+  digitalWrite(LEDPin,LOW);
+  }
+}
