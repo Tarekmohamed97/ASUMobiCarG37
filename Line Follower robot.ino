851dc1d35317cc8988314c8d18b599708cedbca2
عبدالرحمن صبري محمد <36451569+abdosabry@users.noreply.github.com>
2018-04-11 03:36:20 +0200

    https://youtu.be/QL4K7n2c5WM
    ده لينك فسديو حلو اتفرجوا  غليه

---
+++ Line Follower robot.ino
@@ -0,0 +1,47 @@
+#define LS 2 // left sensor
+#define RS 3 // right sensor
+#define LM1 5 // left motor M1a
+#define LM2 4 // left motor M2a
+#define RM1 7 // right motor M2a
+#define RM2 6 // right motor M2b
+void setup()
+{
+pinMode(LS, INPUT);
+pinMode(RS, INPUT);
+pinMode(LM1, OUTPUT);
+pinMode(LM2, OUTPUT);
+pinMode(RM1, OUTPUT);
+pinMode(RM2, OUTPUT);
+}
+void loop()
+{
+if(digitalRead(LS) && digitalRead(RS)) // Move Forward on line
+{
+digitalWrite(LM1, HIGH);
+digitalWrite(LM2, LOW);
+digitalWrite(RM1, HIGH);
+digitalWrite(RM2, LOW);
+}
+if(digitalRead(LS) && !(digitalRead(RS))) // turn left by rotationg left motors in forward and right ones in backward direction
+{
+digitalWrite(LM1, HIGH);
+digitalWrite(LM2, LOW);
+digitalWrite(RM1, LOW);
+digitalWrite(RM2, HIGH);
+}
+if(!(digitalRead(LS)) && digitalRead(RS)) // Turn right by rotating right motors in forward and left ones in backward direction
+{
+digitalWrite(LM1, LOW);
+digitalWrite(LM2, HIGH);
+digitalWrite(RM1, HIGH);
+digitalWrite(RM2, LOW);
+}
+
+if(!(digitalRead(LS)) && !(digitalRead(RS))) // Finish line, stop both the motors
+{
+digitalWrite(LM1, LOW);
+digitalWrite(LM2, LOW);
+digitalWrite(RM1, LOW);
+digitalWrite(RM2, LOW);
+}
+}
